b3f16efc942233ee8779205a99a21b5261998988
Nader Nasr <nadernasr7@gmail.com>
2022-01-12 07:10:37 -0500

    find the middle element in an array

---
+++ middle.js
@@ -0,0 +1,38 @@
+const eqArrays = (firstArr, secondArr) => {
+
+  if (firstArr === secondArr) return true;
+  if (firstArr === null || secondArr === null) return false;
+  if (firstArr.length !== secondArr.length) return false;
+
+  for (let i = 0; i < firstArr.length; ++i) {
+    if (firstArr[i] !== secondArr[i]) return false;
+  }
+  return true;
+
+};
+
+const assertArraysEqual = (firstArr, secondArr) => {
+  if (eqArrays(firstArr, secondArr)) {
+    return `🟢🟢🟢 Assertion Passed: ${firstArr} === ${secondArr} `;
+  } else {
+    return `🔴🔴🔴 Assertion Failed: ${firstArr} !== ${secondArr} `;
+  }
+};
+
+
+
+const middle = (arr) => {
+  let result = [];
+  if (arr.length === 0 || arr.length === 1 || arr.length === 2) {
+    return [];
+  } else if (arr.length % 2 === 1) {
+    let even = (arr.length - 1) / 2;
+    result.push(arr[even]);
+    return result;
+  } else if (arr.length % 2 === 0) {
+    let odd = (arr.length / 2) - 1;
+    let odd2 = (arr.length / 2);
+    result.push(arr[odd], arr[odd2]);
+    return result;
+  }
+};
