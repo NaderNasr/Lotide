3bd82beb27aaa07ae234ded0b001ab151dbc0e7e
Nader Nasr <nadernasr7@gmail.com>
2022-01-12 19:14:27 -0500

    refactor

--- eqObjects.js
+++ eqObjects.js
@@ -12,10 +12,13 @@ const eqArrays = (firstArr, secondArr) => {
 };
 
 const eqObjects = (object1, object2) => {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+  let obj1 = Object.keys(object1);
+  let obj2 = Object.keys(object2);
+
+  if (obj1.length !== obj2.length) {
     return false;
   } else {
-    for (const key of Object.keys(object1)) {
+    for (const key of obj1) {
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
