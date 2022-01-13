cec2e5ac44bb9726812af7d94950de0a0ebbd34b
Nader Nasr <nadernasr7@gmail.com>
2022-01-13 17:12:02 -0500

    push to new array until cb is equal false

---
+++ takeUntil.js
@@ -0,0 +1,23 @@
+const takeUntil = (array, cb) => {
+  let result = [];
+  // array.map((x) =>
+  for (let i = 0; i < array.length; i++) {
+    if (!cb(array[i])) {
+      result.push(array[i]);
+    } else {
+      return result;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
