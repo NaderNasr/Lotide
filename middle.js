3af14111a755640bb3e318e6104d3dda4a2d61d8
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 09:56:15 -0500

    refactored middle

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = (firstArr, secondArr) => {
-
-  if (firstArr === secondArr) return true;
-  if (firstArr === null || secondArr === null) return false;
-  if (firstArr.length !== secondArr.length) return false;
-
-  for (let i = 0; i < firstArr.length; ++i) {
-    if (firstArr[i] !== secondArr[i]) return false;
-  }
-  return true;
-
-};
-
-const assertArraysEqual = (firstArr, secondArr) => {
-  if (eqArrays(firstArr, secondArr)) {
-    return `🟢🟢🟢 Assertion Passed: ${firstArr} === ${secondArr} `;
-  }
-  return `🔴🔴🔴 Assertion Failed: ${firstArr} !== ${secondArr} `;
-  
-};
-
-
-
 const middle = (arr) => {
   let result = [];
   if (arr.length === 0 || arr.length === 1 || arr.length === 2) {
@@ -39,4 +16,4 @@ const middle = (arr) => {
   }
 };
 
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]));
+module.exports = middle;
