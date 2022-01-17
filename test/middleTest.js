6c11f4b2f1905c09e4fa1ccbbdb679c9253bcb42
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 13:40:15 -0500

    refactored middle to mocha testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.deepStrictEqual(middle([1,2,3,4,5]), [3]);
+  });
 
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]));
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [1]));
-console.log(assertArraysEqual(middle([1,2,100,4,5]), [100]));
+  it("returns [3] equal to [1]", () => {
+    assert.deepStrictEqual(middle([1,2,3,4,5]), [3]);
+  });
+
+  it("returns '5' NOT equal to ['6']", () => {
+    assert.notDeepEqual(middle([1,2,100,4,5]), [10]);
+  });
+
+});
