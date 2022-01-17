68a2ad0518599b0f492a48989f7c29b6bb72b61f
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 14:52:43 -0500

    exporting all functions

---
+++ index.js
@@ -0,0 +1,35 @@
+const map = require('./map');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const findKey = require('./findKey');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const takeUntil = require('./takeUntil');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
+const letterPosition = require('./letterPosition');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+
+
+module.exports = {
+  map,
+  head,
+  tail,
+  middle,
+  without,
+  findKey,
+  eqArrays,
+  countOnly,
+  takeUntil,
+  eqObjects,
+  countLetters,
+  findKeyByValue,
+  letterPosition,
+  assertArraysEqual,
+  assertObjectsEqual,
+};
