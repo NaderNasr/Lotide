1ed552cd4f9fa61e6fa5a1041c2c90a7c44d0fd6
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 13:52:04 -0500

    removed comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -13,12 +13,3 @@ describe("#tail", () => {
   });
 
 });
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// console.log(assertEqual(result.length, 2));
-// console.log(assertEqual(result[0], "Lighthouse"));
-// console.log(assertEqual(result[1], "Labs"));
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// console.log(assertEqual(words.length, 3));
