0186679f00f9c40d88cacc1345d6f956a8680798
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 13:51:38 -0500

    refactored to mocha testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,16 +2,16 @@ const assert = require('chai').assert;
 const middle = require('../middle');
 
 describe("#middle", () => {
-  it("returns 1 for [1, 2, 3]", () => {
+  it("returns [3] for [1,2,3,4,5]", () => {
     assert.deepStrictEqual(middle([1,2,3,4,5]), [3]);
   });
 
-  it("returns [3] equal to [1]", () => {
-    assert.deepStrictEqual(middle([1,2,3,4,5]), [3]);
+  it("returns [100] for [1,2,100,4,5]", () => {
+    assert.deepStrictEqual(middle([1,2,100,4,5]), [100]);
   });
 
-  it("returns '5' NOT equal to ['6']", () => {
-    assert.notDeepEqual(middle([1,2,100,4,5]), [10]);
+  it("returns [100] NOT equal to [5]", () => {
+    assert.notDeepEqual(middle([1,2,100,4,5]), [5]);
   });
 
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2));
-console.log(assertEqual(result[0], "Lighthouse"));
-console.log(assertEqual(result[1], "Labs"));
+describe("#tail", () => {
+  it("returns 2 for Lighthouse, Labs", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log(assertEqual(words.length, 3));
+  it("returns Lighthouse", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+
+});
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// console.log(assertEqual(result.length, 2));
+// console.log(assertEqual(result[0], "Lighthouse"));
+// console.log(assertEqual(result[1], "Labs"));
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// console.log(assertEqual(words.length, 3));
