6170702546be1e46861709d89af4da5aa358661e
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 09:05:26 -0500

    refactoring tail

--- tail.js
+++ tail.js
@@ -1,23 +1,5 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    return `🟢🟢🟢 Assertion Passed: ${actual} === ${expected} `;
-  }
-  return `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected} `;
-  
-};
-
 const tail = (arr) => {
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2)); // ensure we get back two elements
-console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3));
-
-console.log(tail([1,2,3,4,5]));
-console.log();
+module.exports = tail;
