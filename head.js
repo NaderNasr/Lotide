db74ba635552bf7cba4610808df17f3886ec71a4
Nader Nasr <nadernasr7@gmail.com>
2022-01-17 09:00:33 -0500

    refactoring head

--- head.js
+++ head.js
@@ -1,16 +1,6 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    return `🟢🟢🟢 Assertion Passed: ${actual} === ${expected} `;
-  }
-  return `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected} `;
-  
-};
-
 const head = (arr) => {
   return arr[0];
 };
 
-console.log(assertEqual(head(["Hello", "LHL", "How's", "it", "going"]), "Hello"));
-console.log(assertEqual(head(["Hello", "LHL", "How's", "it", "going"]), "yessss"));
-console.log(assertEqual(head([1,2,3]), 1));
-console.log(assertEqual(head([2,4,6]), 0));
+module.exports = head;
+
